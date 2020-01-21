33a4498b94ffd98f316f64526de7d93976c97538
유진수 <linebird71@gmail.com>
2020-01-21 15:29:39 +0900

    Update settings.sh

--- provisioning/settings.sh
+++ provisioning/settings.sh
@@ -5,13 +5,16 @@
 #OKD Version
 export OKD_VERSION=3.11
 
+#OKD KT ucloudbiz publicIP
+export KT_PUBLIC_IP=211.254.214.5
+
 #OKD Master Node Configuration
 export OKD_MASTER_IP=172.27.0.144
-export OKD_MASTER_HOSTNAME=oc-master
+export OKD_MASTER_HOSTNAME=oc-master.csa343cloud.internal
 
 #OKD Worker Node 1 Configuration
 export OKD_WORKER_NODE_1_IP=172.27.0.5
-export OKD_WORKER_NODE_1_HOSTNAME=oc-worker
+export OKD_WORKER_NODE_1_HOSTNAME=oc-worker.csa343cloud.internal
 
 #OKD Worker Node 2 Configuration
 #export OKD_WORKER_NODE_2_IP=100.10.10.102
@@ -35,7 +38,7 @@ export API_PORT=8443
 #OKD Login Credentials
 #By default admin/admin operator will be created and can be used to login to OKD console.
 export OKD_USERNAME=admin
-export OKD_PASSWORD=admin
+export OKD_PASSWORD=Arsenal!
 
 #OKD Add-Ons
 #Enable "True"  only if one of the VM has 4GB memory.
