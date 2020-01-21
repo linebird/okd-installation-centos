6d0d04b6ee99076a1bb8e285b0fb11a20ca9d0a0
유진수 <linebird71@gmail.com>
2020-01-21 16:05:53 +0900

    Update install_master.sh

--- provisioning/install_master.sh
+++ provisioning/install_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source settings.sh
+source /root/okd-installation-centos/provisioning/settings.sh
 
-envsubst < inventory.download > inventory.ini
+envsubst < /root/okd-installation-centos/provisioning/inventory.download > /root/okd-installation-centos/provisioning/inventory.ini
 
 # install the packages for Ansible
 #yum -y --enablerepo=epel install ansible pyOpenSSL
@@ -19,10 +19,10 @@ mkdir -p /etc/origin/master/
 touch /etc/origin/master/htpasswd
 
 # check pre-requisites
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i /root/okd-installation-centos/provisioning/inventory.ini openshift-ansible/playbooks/prerequisites.yml
 
 # deploy cluster
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook -i /root/okd-installation-centos/provisioning/inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
 
 htpasswd -b /etc/origin/master/htpasswd $OKD_USERNAME ${OKD_PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin $OKD_USERNAME
